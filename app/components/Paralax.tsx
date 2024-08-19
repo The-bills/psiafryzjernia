2ab05f3cad39fe463adcded40ce18f419e706702
Solucky2 <oneksiakprzemek@gmail.com>
2024-08-19 19:48:29 +0200

    justify center image

--- app/components/Paralax.tsx
+++ app/components/Paralax.tsx
@@ -30,7 +30,7 @@ export const Paralax = () => {
         backgroundSize: 'cover',
         zIndex: -1,
       }}
-      className="h-96 bg-fixed object-fill"
+      className="h-96 bg-fixed object-fill justify-center items-center flex"
     >
     </div>
   );
