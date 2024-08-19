f57d258ddf95446b035553ec48a2e7ab56af5d18
Solucky2 <oneksiakprzemek@gmail.com>
2024-08-19 19:55:13 +0200

    navbar color fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -35,13 +35,13 @@ export const Navbar = () => {
         <nav className="flex flex-col md:flex-row w-full sticky top-0 justify-end bg-bone1 items-center text-lg z-50" style={{overflow: 'hidden'}}>
           <div className="flex w-full justify-between items-center p-4 md:p-0">
             <img src="/logo.svg" alt="logo" className="hidden md:block h-20 mr-auto ml-10" />
-            <button className="md:hidden" onClick={toggleMenu}>
+            <button className="md:hidden " onClick={toggleMenu} style={{color:"black"}}>
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
               </svg>
             </button>
           </div>
-          <div className={`flex flex-col md:flex-row md:items-center ${isOpen ? 'block' : 'hidden'} w-full md:w-auto`}>
+          <div className={`flex flex-col md:flex-row md:items-center ${isOpen ? 'block' : 'hidden'} w-full md:w-auto text-stone-950`}>
             <Element onClick={handleLinkClick} href='#intro' label="Psia fryzjernia"/>
             <Element onClick={handleLinkClick} href='#about' label="O nas"/>
             <Element onClick={handleLinkClick} href='#offer' label="Oferta"/>
