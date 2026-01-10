b784450e8d262ac153c2c804e4b362503a884eca
Solucky2 <oneksiakprzemek@gmail.com>
2026-01-10 13:57:54 +0100

    navbar 1st fix

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -37,13 +37,13 @@ export const Navbar = () => {
           <div className="flex w-full md:justify-between justify-start md:gap-0 gap-8 items-center p-4 md:p-0">
             <img src="/logo.svg" alt="logo" className="hidden md:block h-16 ml-8" />
             <img src="logo_pl_200.png" alt="orly_groomingu" className="md:hidden h-10"/>
-            <button className="md:hidden ml-auto" onClick={toggleMenu} style={{color:"black"}}>
+            <button className="md:hidden ml-auto transition-transform duration-150 active:scale-90" onClick={toggleMenu} style={{color:"black"}}>
               <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
               </svg>
             </button>
           </div>
-          <div className={`flex flex-col md:flex-row md:items-center md:gap-3 ${isOpen ? 'block' : 'hidden'} w-full md:w-auto text-graphite1`}>
+          <div className={`flex flex-col md:flex-row md:items-center md:gap-3 ${isOpen ? 'block' : 'hidden'} w-full md:w-auto text-graphite1 text-right mb-8 md:mb-0`}>
             <Element onClick={handleLinkClick} href='#intro' label="Psia fryzjernia"/>
             <Element onClick={handleLinkClick} href='#about' label="O nas"/>
             <Element onClick={handleLinkClick} href='#offer' label="Oferta"/>
@@ -54,7 +54,7 @@ export const Navbar = () => {
             <Element onClick={handleLinkClick} href="#opinions" label="Opinie"/>
             <Element onClick={handleLinkClick} href="#contact" label="Kontakt"/>
           </div>
-          <img src="logo_pl_200.png" alt="orly_groomingu" className="hidden md:block h-12 mr-auto ml-8 mr-8" />
+          <img src="logo_pl_200.png" alt="orly_groomingu" className="hidden md:block h-12 ml-8 mr-8" />
         </nav>
       );
 };
